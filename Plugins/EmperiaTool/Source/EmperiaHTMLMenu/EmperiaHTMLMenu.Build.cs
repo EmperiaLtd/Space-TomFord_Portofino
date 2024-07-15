3099168cc5ac8e17b598d8dfa590fff38dd6fb4f
tomastamulenas <65013391+tomastamulenas@users.noreply.github.com>
2024-07-15 12:59:35 +0300

    CREATED SPACE

---
+++ Plugins/EmperiaTool/Source/EmperiaHTMLMenu/EmperiaHTMLMenu.Build.cs
@@ -0,0 +1,87 @@
+﻿using UnrealBuildTool;
+
+public class EmperiaHTMLMenu : ModuleRules
+{
+    public EmperiaHTMLMenu(ReadOnlyTargetRules Target) : base(Target)
+    {
+	    bUsePrecompiled=true;
+	    PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicIncludePaths.AddRange(
+			new string[] {
+				// ... add public include paths required here ...
+			}
+			);
+
+
+		PrivateIncludePaths.AddRange(
+			new string[] {
+				// ... add other private include paths required here ...
+			}
+			);
+
+
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore",
+                "UMG",
+                "WebBrowser",
+                "Networking",
+                "Sockets",
+                "Json", 
+                "JsonUtilities"
+            }
+			);
+
+
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"CoreUObject",
+				"Engine",
+				"Slate",
+				"SlateCore",
+                "UMG",
+                "WebBrowser",
+                "Networking",
+                "Sockets",
+                "Json", 
+                "JsonUtilities"
+			}
+			);
+
+
+		DynamicallyLoadedModuleNames.AddRange(
+			new string[]
+			{
+			}
+			);
+
+		if (Target.bBuildEditor == true)
+        {
+            PrivateIncludePathModuleNames.AddRange(
+                new string[]
+                {
+                        "UnrealEd",
+                }
+            );
+            PrivateDependencyModuleNames.AddRange(
+                new string[]
+                {
+                        "UnrealEd",
+                }
+            );
+        }
+		/*PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"WebBrowserTexture",
+			}
+		);*/
+    }
+}
---
+++ Plugins/EmperiaTool/Source/EmperiaToolEditor/EmperiaToolEditor.Build.cs
@@ -0,0 +1,65 @@
+﻿using UnrealBuildTool;
+
+public class EmperiaToolEditor : ModuleRules
+{
+    public EmperiaToolEditor(ReadOnlyTargetRules Target) : base(Target)
+    {
+        bUsePrecompiled=true;
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+        
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+                "UMG",
+                "UMGEditor",
+                "Blutility",
+                "MaterialEditor",
+                "CoreUObject",
+                "JsonUtilities",
+                "MediaAssets", 
+                "WebBrowserWidget", 
+                "PlacementMode",
+                "LevelEditor",
+                "SlateCore",
+                "OpenSSL",
+                "UnrealEd",
+                "WebBrowser",
+                "Networking",
+                "Sockets"
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Projects",
+                "CoreUObject",
+                "InputCore",
+                "MaterialEditor",
+                "UnrealEd",
+                "ToolMenus",
+                "Engine",
+                "Slate",
+                "SlateCore",
+                "MessageLog",
+                "HTTP",
+                "HTTPServer",
+                "Json",
+                "JsonUtilities",
+                "EditorScriptingUtilities", 
+                "ImageWriteQueue",
+                "WebBrowser", 
+                "WebBrowserTexture",
+                "ImageWrapper",
+                "RenderCore",
+                "OpenSSL",
+                "MovieRenderPipelineCore",
+                "UMG",
+                "WebBrowser",
+                "Networking",
+                "Sockets"
+            }
+        );
+    }
+}
